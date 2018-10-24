25aad82a8b38f2ad2bb3fa7df02001140ec8eeac
Mironov Alex <mason1988@mail.ru>
2018-10-24 09:41:38 +0300

    Initial commit

---
+++ src/Work.java
@@ -0,0 +1,104 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+public class Work {
+    private static Set<String> blacList;
+    private static List<String> text;
+    public static void start(){
+        setBlacList();
+        proverka();
+        itog();
+    }
+
+    private static void setBlacList() {
+        blacList = new TreeSet<>();
+        try (FileReader reader = new FileReader("blacList.txt")) {
+            String line = new String();
+            int c;
+            while ((c = reader.read()) != -1) {
+                if ((char) c == '\r' || (char) c == '\n') {
+                    line += " ";
+                } else {
+                    if ((char) c == '.' || (char) c == ',') {
+                        line = line.trim();
+                        line = line.toLowerCase();
+                        blacList.add(line);
+                        line = new String();
+                    } else {
+                        line += (char) c;
+                    }
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("===Цензура===");
+        System.out.println(blacList);
+        System.out.println("==================");
+    }
+
+        private static void proverka(){
+            text = new ArrayList<>();
+            try (FileReader reader = new FileReader("text.txt")) {
+                String line = new String();
+                int c;
+                while ((c = reader.read()) != -1) {
+                    if ((char) c == '\r' || (char) c == '\n') {
+                        line += " ";
+                    } else {
+                        if ((char) c == '.' || (char) c == '!'|| (char) c == '?') {
+                            //line += (char) c;
+                            line = line.trim();
+                            search(line);
+                            line = new String();
+
+                        } else {
+                            line += (char) c;
+                        }
+                    }
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+//    private static void search(String line) {
+//        String line2;
+//        line2 = line.toLowerCase();
+//
+//        for (String t:blacList) {
+//            if (line2.contains(t)){
+//                text.add(line);
+//                break;
+//            }
+//        }
+//    }
+    private static void search(String line){
+        String line2;
+        line2 = line.toLowerCase();
+        String [] mas = line2.split("\\s*(\\s|,)\\s*");
+        ArrayList<String> arrayList; ;
+        arrayList=new ArrayList<>(Arrays.asList(mas));
+        for (String s:arrayList) {
+           if(blacList.contains(s)){
+               text.add(line);
+               break;
+           }
+        }
+    }
+
+
+    private static void itog(){
+        if (text.size()==0){
+            System.out.println("Текст проше проверку на цензуру");
+        }else {
+            System.out.println("Количество предложений который надо исправить: "+text.size());
+            System.out.println(text);
+        }
+    }
+}
+
